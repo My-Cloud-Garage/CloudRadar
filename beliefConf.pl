c9a94944d1f7425ddf15e4b91e0c79c04acaeb5f
CloudGarageAdmin <33375644+BetterWorldCloud@users.noreply.github.com>
2017-12-23 16:38:03 -0600

    Delete beliefConf.pl

--- beliefConf.pl
+++
@@ -1,4 +0,0 @@
-naclEntry('acl-899f58f2','0.0.0.0/0','egress','allow','100','-1','-1').
-naclEntry('acl-899f58f2','0.0.0.0/0','egress','deny','32767','-1','-1').
-naclEntry('acl-899f58f2','0.0.0.0/0','ingress','allow','100','-1','-1').
-naclEntry('acl-899f58f2','0.0.0.0/0','ingress','deny','32767','-1','-1').
